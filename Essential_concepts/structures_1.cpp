204461af9f1a6e2d0e7ede0205eb4f539fbf093e
Yashraj <40593311+yashraj-code@users.noreply.github.com>
2023-05-17 16:27:45 +0530

    structures basic

---
+++ Essential_concepts/structures_1.cpp
@@ -0,0 +1,25 @@
+<// <---------- RECTANGLE EXAMPLE 1 ---------->
+
+#include<stdio.h>
+#include<iostream>
+
+using namespace std;
+
+struct rectangle
+{
+    int length;
+    int breadth;
+};
+
+int main()
+{
+    struct rectangle r1 = {10,5}; // DEFAULT VALUE
+
+    r1.length=10;
+    r1.breadth=28;
+
+    cout<<r1.length<<endl;
+    cout<<r1.breadth<<endl;
+
+    return 0;
+}
