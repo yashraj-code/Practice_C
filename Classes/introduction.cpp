74d445199abb5389b053ea63b03b29e955678862
Yashraj <40593311+yashraj-code@users.noreply.github.com>
2023-05-23 23:18:04 +0530

    classes

---
+++ Classes/introduction.cpp
@@ -0,0 +1,22 @@
+class Rectangle 
+{
+
+private:
+    int length;
+    int width;
+
+
+public:
+    void setLength(int l) 
+		{
+        length = l;
+    }
+    void setWidth(int w) 
+		{
+        width = w;
+    }
+    int area() 
+		{
+        return length * width;
+    }
+};
