9c3aa44ea4b091255906a1468db0838cfe345e46
Yashraj <40593311+yashraj-code@users.noreply.github.com>
2023-09-18 22:31:54 +0530

    traverse binary tree in post order fashion

---
+++ Tree_binaryTree/Binary_Tree/10_BT_traversal_postorder.cpp
@@ -0,0 +1,88 @@
+/*
+Traverse the left subtree, call Postorder(left->subtree)
+Traverse the right subtree, call Postorder(right->subtree)
+Visit the root.
+*/
+
+// AB+
+
+/*
+              1
+             / \
+            2   3
+           / \  / \
+          4   5 6  7
+*/
+
+#include<stdio.h>
+#include<iostream>
+#include<queue>
+#include "BinaryTreeNode.h"
+
+using namespace std;
+
+BinaryTreeNode<int> * Take_level_input()
+{
+    int rootData;
+    cout<<"Enter Root Data ->" <<endl;
+    cin>>rootData;
+
+    if(rootData == -1)
+    {return NULL;}
+
+    //create node
+    BinaryTreeNode<int>* root = new BinaryTreeNode<int>(rootData);
+
+    //create a queue
+    queue<BinaryTreeNode<int>*> pendingNodes;
+    pendingNodes.push(root);
+
+    while(pendingNodes.size()!=0)
+    {
+        //pop out the first element
+        BinaryTreeNode<int>* front =pendingNodes.front();
+        pendingNodes.pop();
+        
+        //For LEFT CHILD
+        cout<<"\nEnter Left Child of "<<front->data<<"-> ";
+        int leftChildData;
+        cin>>leftChildData;
+        if(leftChildData != -1)
+        {
+            BinaryTreeNode<int>* leftChild = new BinaryTreeNode<int>(leftChildData);
+            front->left = leftChild;
+            pendingNodes.push(leftChild);
+        }
+
+        //For RIGHT CHILD
+        cout<<"\nEnter right Child of "<<front->data<<"-> ";
+        int rightChildData;
+        cin>>rightChildData;
+        if(rightChildData != -1)
+        {
+            BinaryTreeNode<int>* rightChild = new BinaryTreeNode<int>(rightChildData);
+            front->right = rightChild;
+            pendingNodes.push(rightChild);
+        }
+    }
+
+    return root;
+}
+
+void PostOrderTraversal(BinaryTreeNode<int>* root)
+{
+    if(root==NULL)
+        {return;}
+    
+    PostOrderTraversal(root->left);
+    PostOrderTraversal(root->right);
+    cout<<root->data<<" ";
+}
+
+
+int main()
+{
+BinaryTreeNode<int>* root = Take_level_input();
+PostOrderTraversal(root);
+
+}
