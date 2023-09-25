c9c0a1d4a8d02d435802384e9ca7ec8565e61473
Yashraj <40593311+yashraj-code@users.noreply.github.com>
2023-09-25 21:00:30 +0530

    Added Comments

--- Tree_binaryTree/Complete_binary_tree.cpp
+++ Tree_binaryTree/Complete_binary_tree.cpp
@@ -1,8 +1,32 @@
 // Checking if a binary tree is a complete binary tree in C++
+//solution_3 (Recursive)
 
 /*
 A complete binary tree is a binary tree in which all the levels are completely filled 
 except possibly the lowest one, which is filled from the left.
+*/
+
+/*
+In the array representation of a binary tree, 
+if the parent node is assigned an index of ‘i’ and 
+left child gets assigned an index of ‘2*i + 1’ while 
+the right child is assigned an index of ‘2*i + 2’. 
+
+If we represent the above binary tree as an array with the respective indices 
+assigned to the different nodes of the tree above from top to down and left to right.
+
+    Hence we proceed in the following manner in order to check if the binary tree is complete binary tree. 
+
+    1. Calculate the number of nodes (count) in the binary tree.
+    2. Start recursion of the binary tree from the root node of the binary tree with index (i) being set as 0 
+		and the number of nodes in the binary (count).
+    3. If the current node under examination is NULL, then the tree is a complete binary tree. Return true.
+    4. If index (i) of the current node is greater than or equal to the number of nodes in the binary tree (count) 
+		i.e. (i>= count), then the tree is not a complete binary. Return false.
+    5. Recursively check the left and right sub-trees of the binary tree for same condition. 
+		For the left sub-tree use the index as (2*i + 1) while 
+		for the right sub-tree use the index as (2*i + 2).
+
 */
 
 
